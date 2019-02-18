8761324e50678e7fb70dcfe71fb4031daea6ad67
George Aristy <george.aristy@gmail.com>
2019-02-18 18:23:57 -0500

    (#6) Fail build if coverage is low

--- check_coverage.sh
+++ check_coverage.sh
@@ -1,46 +1,33 @@
-#
+#!/bin/bash
+# 
 # Copyright 2019 George Aristy
-#
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# 
 
-#
-# Supported Targets:
-#
-# build:   builds the binary
-# test:    runs tests
-# lint:    runs linters
-# pdd:     runs pdd (see https://github.com/yegor256/pdd)
-# checks:  runs build+test+pdd+lint
-# release: releases to GitHub (requires GitHub token)
-#
+let THRESHOLD=80
 
-build:
-	go build -o gitlint cmd/go-gitlint/main.go
+let exit_code=0
 
-test:
-	go test -count=1 -race -cover -coverprofile=coverage.txt -covermode=atomic ./... | tee cov_check.txt
+while read line; do
+	if [ "$(echo $line | grep coverage)" != "" ]; then
+		pkg=$(echo $line | sed 's/\s\+/ /g' | sed 's/%//' | cut -d ' ' -f 2)
+		cov=$(echo $line | sed 's/\s\+/ /g' | sed 's/%//' | cut -d ' ' -f 5)
+		if [ 1 -eq $(echo "$THRESHOLD > $cov" | bc) ]; then
+			echo "Coverage [$cov] for package [$pkg] is below threshold [$THRESHOLD]"
+			let exit_code++
+		fi
+	fi
+done < ./cov_check.txt
 
-coverage:
-	./check_coverage.sh
-
-lint:
-	./bin/golangci-lint run
-
-pdd:
-	pdd --file=puzzles.xml
-
-checks: build lint pdd test coverage
-
-release:
-	./release.sh
+exit $exit_code
